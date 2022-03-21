a7e70cea17a70de34fb93e86aeef5d3dd1024e16
yangzhengca <yangzhengca>
2022-03-21 14:04:21 -0600

    finished topbar

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { NotificationsNone, Language, Settings } from '@material-ui/icons';
 import "./Topbar.css"
 
 const Topbar = () => {
@@ -10,7 +11,21 @@ const Topbar = () => {
                 SuperAdmin
             </span>
         </div>
-        <div className="topRight">right</div>
+        <div className="topRight">
+            <div className="topbarIconContainer">
+                <NotificationsNone />
+                <span className="topIconBadge">2</span>
+            </div>
+            <div className="topbarIconContainer">
+                <Language />
+                <span className="topIconBadge">2</span>
+            </div>
+            <div className="topbarIconContainer">
+                <Settings />
+                
+            </div>
+            <img src="https://cdn.vox-cdn.com/thumbor/dbSL2rQErtPprh4v_icOepQO9Ls=/0x0:2370x1574/920x613/filters:focal(996x598:1374x976):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/69715362/Screen_Shot_2020_07_21_at_9.38.25_AM.0.png" alt="avator" className="topAvator" />
+        </div>
       </div>
     </div>
   );
